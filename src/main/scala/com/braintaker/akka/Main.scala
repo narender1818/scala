0969d47317ac21d6aacae02a978fc9a9193bb8e6
narender1818 <narender1818@gmail.com>
2016-03-06 22:07:38 -0800

    code change

--- src/main/scala/com/braintaker/akka/Main.scala
+++ src/main/scala/com/braintaker/akka/Main.scala
@@ -1,5 +1,16 @@
 package com.braintaker.akka
 
-class Main extends App  {
-  def main(args: Array[String]) = println("Hi!")
+import com.braintaker.preprocess.Test
+
+object Main extends App {
+
+  def matchTest(x: Any): Any = x match {
+    case "1" =>
+      val ss = new Test()
+      ss.callActor
+    case "2" =>
+      println("args 2 : ")
+
+  }
+  matchTest(args(0))
 }
