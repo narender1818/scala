e81e90c61be5c6f9599044766b29e756b53f4c78
narender1818 <narender1818@gmail.com>
2016-05-31 09:33:18 +0530

    sss

--- src/main/scala/com/braintaker/preprocess/YahooFinancials.scala
+++
@@ -1,9 +0,0 @@
-package com.braintaker.preprocess
-
-object YahooFinancials extends Enumeration {
-  type YahooFinancials = Value
-  val DATE, OPEN, HIGH, LOW, CLOSE, VOLUME, ADJ_CLOSE = Value
-  val volatility = (fs: Array[String]) => fs(HIGH.id).toDouble - fs(LOW.
-    id).toDouble
-
-}
