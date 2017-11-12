8496626275fa64fa72e27d2b16ca1e7baa7c297f
narender1818 <narender1818@gmail.com>
2017-11-12 22:19:41 +0530

    util change

--- src/main/scala/com/bt/util/Util.scala
+++ src/main/scala/com/bt/util/Util.scala
@@ -5,8 +5,22 @@ package com.bt.util
   */
 trait Util {
 
-  //def getTimeStamp():Long={
-  //  4.7L
-  //}
+  def getTimeStamp():Long={
+    4.7L
+  }
 
 }
+
+/*class Util {
+  def readFile(file: String): String = {
+    scala.io.Source.fromFile(file).mkString
+  }
+  def readProperty(path: String): Properties = {
+    val prop = new Properties()
+    val conf: Configuration = new Configuration()
+    val fileSystem: FileSystem = FileSystem.get(URI.create(path), conf)
+    val inputStream: InputStream = fileSystem.open(new Path(path))
+    prop.load(inputStream)
+    return prop
+  }
+}*/
